87360c05cd70e3125e09a513c23c9bdf4a84c00b
jsuarezbaron <jsuarezbaron@gmail.com>
2019-05-23 19:48:41 -0300

    Se grega solución de separación de datos y clasisicación para el primer problema

--- TP1/TP1.py
+++ TP1/TP1.py
@@ -3,44 +3,78 @@ import numpy as np
 import matplotlib.pyplot as plt
 from perceptron_multicapa import PerceptronMulticapa
 
-# X1 = np.genfromtxt(fname='tp1_ej1_training.csv', delimiter=',',dtype=float, usecols=(1,2,3,4,5,6,7,8,9,10))
-# y1 = np.genfromtxt(fname='tp1_ej1_training.csv', delimiter=',',dtype=str, usecols=0)
-# y1 = np.where(y1 == 'M', 0.95, 0.05)
+X = np.genfromtxt(fname='tp1_ej1_training.csv', delimiter=',',dtype=float, usecols=(1,2,3,4,5,6,7,8,9,10))
+y = np.genfromtxt(fname='tp1_ej1_training.csv', delimiter=',',dtype=str, usecols=0)
+y = np.where(y == 'M', 0.95, 0.05)	###Decidimos convertir las 'M' a 0,95 (a modo de 1) y las 'B' a 0.05 (a mode de 0) ya que la sigmoidea no alcanza los valores 1 y 0.
 
-# for i in range(10):
-# 	mean = np.mean(X1[:,i])
-# 	std = np.std(X1[:,i])
-# 	X1[:,i] -= mean
-# 	X1[:,i] /= std
+for i in range(10):
+	mean = np.mean(X[:,i])
+	std = np.std(X[:,i])
+	X[:,i] -= mean
+	X[:,i] /= std
 
-# ppn1 = PerceptronMulticapa(10,10,1)
-# ppn1.train(X1, y1, epochs=500, eta=0.1, umbral=0.005)
+XTrain = X[0:328,:] ###80% training, 10% validation y 10% testing
+yTrain = y[0:328]
+XValid = X[328:369,:]
+yValid = y[328:369]
+XTest = X[369:,:]
+yTest = y[369:]
 
-# plt.plot(range(1, len(ppn1.errors_)+1), ppn1.errors_, marker='o')
-# plt.xlabel('Epocas')
-# plt.ylabel('Clasificaciones erroneas')
-# plt.show()
+np.random.seed(2)
+ppn1 = PerceptronMulticapa(10,10,1)		###El primer argumento corresponde a la cantidad de entradas, 
+###el segundo corresponde a la cantidad de neuronas de la capa oculta y el tercero a la cantidad de salidas.
+ppn1.train(XTrain, yTrain, XValid, yValid, epochs=500, eta=0.1, umbral=10**-10)
 
-X2 = np.genfromtxt(fname='tp1_ej2_training.csv', delimiter=',',dtype=float, usecols=(0,1,2,3,4,5,6,7))
-y2 = np.genfromtxt(fname='tp1_ej2_training.csv', delimiter=',',dtype=float, usecols=(8,9))
-
-for i in range(8):
-	mean = np.mean(X2[:,i])
-	std = np.std(X2[:,i])
-	X2[:,i] -= mean
-	X2[:,i] /= std
-
-for i in range(2):
-	m = np.min(y2[:,i])
-	M = np.max(y2[:,i])
-	y2[:,i] -= m
-	y2[:,i] /= (M - m)
-
-plt.plot()
-ppn2 = PerceptronMulticapa(8,8,2,beta=0.5)
-ppn2.train(X2, y2, epochs=800, eta=0.01, umbral=0.008)
-
-plt.plot(range(1, len(ppn2.errors_)+1), ppn2.errors_, marker='o')
+plt.plot(range(1, len(ppn1.errorsTrain_)+1), ppn1.errorsTrain_,color='b',label='Errores training')
+plt.plot(range(1, len(ppn1.errorsValid_)+1), ppn1.errorsValid_,color='r',label='Errores validation')
+plt.legend()
+plt.grid()
 plt.xlabel('Epocas')
 plt.ylabel('Clasificaciones erroneas')
 plt.show()
+
+testing = np.array([])
+for i in range(len(yTest)):
+	testing = np.append(testing,ppn1.forward(XTest[i,:], redEntrenada=True, w1=ppn1.w1Val, w2=ppn1.w2Val))
+
+testing = np.where(testing > 0.5, 0.95, 0.05)
+
+matrizConfusion = np.zeros([2,2])
+
+for i in range(len(yTest)):
+	if yTest[i] == 0.95:
+		if yTest[i] == testing[i]:
+			matrizConfusion[0,0] += 1
+		else:
+			matrizConfusion[0,1] += 1
+	else:
+		if yTest[i] == testing[i]:
+			matrizConfusion[1,1] += 1
+		else:
+			matrizConfusion[1,0] += 1
+
+print(matrizConfusion)
+
+# X = np.genfromtxt(fname='tp1_ej2_training.csv', delimiter=',',dtype=float, usecols=(0,1,2,3,4,5,6,7))
+# y = np.genfromtxt(fname='tp1_ej2_training.csv', delimiter=',',dtype=float, usecols=(8,9))
+
+# for i in range(8):
+# 	mean = np.mean(X[:,i])
+# 	std = np.std(X[:,i])
+# 	X[:,i] -= mean
+# 	X[:,i] /= std
+
+# for i in range(2):	###Decidimos usar este rescaling (min-max) ya que utilizar estandarización podría permitir datos por encima de 1 y por debajo de 0, valores que la sigmoidea no alcanza
+# 	m = np.min(y[:,i])
+# 	M = np.max(y[:,i])
+# 	y[:,i] -= m
+# 	y[:,i] /= (M - m)
+
+# plt.plot()
+# ppn2 = PerceptronMulticapa(8,8,2)
+# ppn2.train(X, y, epochs=1000, eta=0.01, umbral=0.007)
+
+# plt.plot(range(1, len(ppn2.errors_)+1), ppn2.errors_, marker='o')
+# plt.xlabel('Epocas')
+# plt.ylabel('Clasificaciones erroneas')
+# plt.show()
--- TP1/perceptron_multicapa.py
+++ TP1/perceptron_multicapa.py
@@ -19,13 +19,16 @@ class PerceptronMulticapa(object):
     	self.delta1_ = np.zeros(neurons)
     	self.delta2_ = np.zeros(outputs)
 
-    def forward(self, x):
-    	mu = np.append(x,1)
-    	for j in range(self.neu_):
-    		self.v_[j] = self.predict(mu,self.w1_[j,])
-    	for i in range(self.out_):
-    		self.output_[i] = self.predict(self.v_,self.w2_[i,])
-    	return self.output_
+    def forward(self, x, redEntrenada=False, w1=0, w2=0):
+        if not redEntrenada:
+            w1 = self.w1_
+            w2 = self.w2_
+        mu = np.append(x,1)
+        for j in range(self.neu_):
+            self.v_[j] = self.predict(mu,w1[j,])
+        for i in range(self.out_):
+            self.output_[i] = self.predict(self.v_,w2[i,])
+        return self.output_
 
     def backward(self, x, output, target, eta):
     	mu = np.append(x,1)
@@ -41,13 +44,17 @@ class PerceptronMulticapa(object):
     			self.w1_[i,j] += eta * mu[j] * self.delta1_[i]
     	return 0.5 * sum((target - output)**2)
 
-    def train(self, X, y, eta=0.01, epochs=100, umbral=10 ** -100):
-    	self.eta = eta
-    	self.epochs = epochs
-    	self.errors_ = []
-    	for _ in range(self.epochs):
+    def train(self, XTrain, yTrain, XValid, yValid, eta=0.01, epochs=100, umbral=10 ** -100):
+        self.eta = eta
+        self.epochs = epochs
+        self.errorsTrain_ = []
+        self.errorsValid_ = []
+        self.w1Val = 0
+        self.w2Val = 0
+        minVal= float("inf")
+        for _ in range(self.epochs):
             errors = 0
-            c = list(zip(X, y))
+            c = list(zip(XTrain, yTrain))
             np.random.shuffle(c)
             for mu_i, tar in c:
                 if np.shape(tar)==():
@@ -56,11 +63,24 @@ class PerceptronMulticapa(object):
                     target = np.array(tar)
                 output = self.forward(mu_i)
                 errors += self.backward(mu_i,output,target,self.eta)
-            errors = errors/(np.shape(X)[0])
+            errors = errors/(np.shape(XTrain)[0])
             if errors <= umbral:
                 break
             else:
-                self.errors_.append(errors)
+                self.errorsTrain_.append(errors)
+            errors = 0
+            for x, y in zip(XValid,yValid):
+                output = self.forward(x)
+                if np.shape(output)==():
+                    output = np.array([output])
+                    y = np.array([y])
+                errors += 0.5 * sum((output - y)**2)
+            errors /= (np.shape(XTrain)[0])
+            self.errorsValid_.append(errors)
+            if self.errorsValid_[-1] < minVal:
+                minVal = self.errorsValid_[-1]
+                self.w1Val = self.w1_
+                self.w2Val = self.w2_
 
     def sigmoid(self, X):
         return 1 / (1 + math.exp(-self.beta_*X))
